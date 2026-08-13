bcf2763f0a9c21ce2066eddf78b64d95aca8cfe0
Harish <khharish2@gmail.com>
2026-08-13 09:54:37 +0100

    adding files

---
+++ src/com/daily/neetcodeSolns/ValidAnagram.java
@@ -0,0 +1,40 @@
+package com.daily.neetcodeSolns;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+        System.out.println(
+                new ValidAnagram().isAnagram("racecar", "carrace")
+        );
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (map.containsKey(t.charAt(i))) {
+                if (map.get(t.charAt(i)) > 0) {
+                    map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+
+                    if (map.get(t.charAt(i)) == 0) {
+                        map.remove(t.charAt(i));
+                    }
+
+                } else {
+                    map.remove(t.charAt(i));
+                }
+            }
+        }
+
+
+        return map.isEmpty();
+    }
+}
