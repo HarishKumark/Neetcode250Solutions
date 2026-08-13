adcd7072a3e3bde9b8e1810b94ac758ec80cbcb0
Harish <khharish2@gmail.com>
2026-08-13 10:09:27 +0100

    adding files

---
+++ src/com/daily/neetcodeSolns/TwoSum.java
@@ -0,0 +1,24 @@
+package com.daily.neetcodeSolns;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, 2};
+                    }
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+
+}
